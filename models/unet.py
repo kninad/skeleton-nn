9c29f9644b7ab912583fcbce7bfaa652f4a64f0f
Ninad Khargonkar <ninad.khargonkar@kitware.com>
2022-06-20 08:04:29 -0500

    diff unet archs

--- models/unet.py
+++ models/unet.py
@@ -47,6 +47,38 @@ class Unet2D_simple(nn.Module):
 
         self.down1 = DownBlock(64, 128)
         self.down2 = DownBlock(128, 256)
+        # self.down3 = DownBlock(256, 512)
+        # self.down4 = DownBlock(512, 1024)
+        # self.up1 = UpBlock(1024, 512)
+        # self.up2 = UpBlock(512, 256)
+        self.up3 = UpBlock(256, 128)
+        self.up4 = UpBlock(128, 64)
+
+    def forward(self, x):
+        x1 = self.input_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        # x4 = self.down3(x3)
+        # x5 = self.down4(x4)
+        # x = self.up1(x5, x4)
+        # x = self.up2(x4, x3)
+        x = self.up3(x3, x2)
+        x = self.up4(x, x1)
+        logits = self.output_conv(x)
+        return logits
+
+class Unet2D_org(nn.Module):
+
+    def __init__(self, channels: int, num_class: int) -> None:
+        super(Unet2D_org, self).__init__()
+        self.channels = channels
+        self.num_class = num_class
+        # channels = (1, 64, 128, 256, 512, 1024)
+        self.input_conv = ConvBlock(channels, 64)
+        self.output_conv = FinalBlock(64, num_class)
+
+        self.down1 = DownBlock(64, 128)
+        self.down2 = DownBlock(128, 256)
         self.down3 = DownBlock(256, 512)
         self.down4 = DownBlock(512, 1024)
         self.up1 = UpBlock(1024, 512)
@@ -61,8 +93,8 @@ class Unet2D_simple(nn.Module):
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
+        x = self.up2(x4, x3)
+        x = self.up3(x3, x2)
         x = self.up4(x, x1)
         logits = self.output_conv(x)
         return logits
