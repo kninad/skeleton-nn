69c247967b1a1a7c08da9c0db82ef290c9257aa1
Ninad Khargonkar <ninad.khargonkar@kitware.com>
2022-06-22 22:41:11 -0500

    removed the function based loss and made all the loss functions as nn.Modules

--- utils/loss.py
+++ utils/loss.py
@@ -1,39 +1,37 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as nnFunc
 
 
-def structure_loss(pred, mask):
-    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
-    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
-    wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+class StructuredLoss(nn.Module):
+    def __init__(self) -> None:
+        super(StructuredLoss, self).__init__()
+        self.pool = nn.AvgPool2d(kernel_size=31, stride=1, padding=15)
 
-    pred = torch.sigmoid(pred)
-    inter = ((pred * mask) * weit).sum(dim=(2, 3))
-    union = ((pred + mask) * weit).sum(dim=(2, 3))
-    wiou = 1 - (inter + 1) / (union - inter + 1)
+    def forward(self, pred, mask):
+        weit = 1 + 5 * torch.abs(self.pool(mask) - mask)
+        wbce = nnFunc.binary_cross_entropy_with_logits(
+            pred, mask, reduce='none')
+        wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
 
-    wdiss = 1 - (2*inter + 0.5)/(union +0.5)
-    return (wbce + wiou + wdiss).mean()
+        pred = torch.sigmoid(pred)
+        inter = ((pred * mask) * weit).sum(dim=(2, 3))
+        union = ((pred + mask) * weit).sum(dim=(2, 3))
+        wiou = 1 - (inter + 1) / (union - inter + 1)
+        wdiss = 1 - (2*inter + 0.5)/(union + 0.5)
+        return (wbce + wiou + wdiss).mean()
 
-def dice_loss(pred, mask):
-    weit = 1 + 5*torch.abs(F.avg_pool2d(mask,kernel_size=31,stride=1,padding=15)-mask)
-    pred = torch.sigmoid(pred)
-    inter = ((pred * mask) * weit).sum(dim=(2,3))
-    union = ((pred + mask) * weit).sum(dim=(2,3))
-    wdiss = 1 - (2*inter + 0.5)/(union+0.5)
-    return wdiss.mean()
 
 class DiceLoss(nn.Module):
     def __init__(self) -> None:
         super(DiceLoss, self).__init__()
         self.pool = nn.AvgPool2d(kernel_size=31, stride=1, padding=15)
-    
+
     def forward(self, pred, mask):
-        weit = 1 + 5 * torch.abs(self.pool(mask) - mask) 
+        weit = 1 + 5 * torch.abs(self.pool(mask) - mask)
         pred = torch.sigmoid(pred)
-        inter = ((pred * mask) * weit).sum(dim=(2,3))
-        union = ((pred + mask) * weit).sum(dim=(2,3))
+        inter = ((pred * mask) * weit).sum(dim=(2, 3))
+        union = ((pred + mask) * weit).sum(dim=(2, 3))
         wdiss = 1 - (2*inter + 0.5)/(union+0.5)
         return wdiss.mean()
 
@@ -47,7 +45,8 @@ class WeightedFocalLoss(nn.Module):
         self.gamma = gamma
 
     def forward(self, inputs, targets):
-        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        BCE_loss = nnFunc.binary_cross_entropy_with_logits(
+            inputs, targets, reduction='none')
         targets = targets.type(torch.long)
         at = self.alpha.gather(0, targets.data.view(-1))
         pt = torch.exp(-BCE_loss)
