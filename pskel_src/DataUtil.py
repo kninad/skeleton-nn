7acaf80319bd7129b1ace897dbf644c577539a5f
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2022-08-06 20:53:58 -0500

    leaflet sreps processed dataloader

--- pskel_src/DataUtil.py
+++ pskel_src/DataUtil.py
@@ -91,7 +91,7 @@ class TestBinaryImageData(Dataset):
         return pointSet
 
 
-class HippocampiData(Dataset):
+class HippocampiProcessedData(Dataset):
     def __init__(self, data_list, label_list, point_num, load_in_ram=True):
         self.data_list = data_list
         self.label_list = label_list
@@ -162,7 +162,7 @@ class HippocampiData(Dataset):
             points.append(spoke_pt)
             if 'CrestSpoke' in item:
                 crest_pt = item['CrestSpoke']['SkeletalPoint']['Value']
-            points.append(crest_pt)
+                points.append(crest_pt)
         return points
 
     def _util_get_points_from_skeleton(self, skeleton_list):
@@ -170,3 +170,68 @@ class HippocampiData(Dataset):
         for inner_list in skeleton_list:
             all_points += self._util_get_points_from_inner_list(inner_list)
         return all_points
+
+
+class LeafletData(Dataset):
+    def __init__(self, data_list, label_list, point_num, load_in_ram=True):
+        self.data_list = data_list
+        self.label_list = label_list
+        if len(data_list) != len(label_list):
+            raise AssertionError("Data and Label lists dont have same lengths.")
+        self.point_num = point_num
+        self.load_in_ram = load_in_ram
+        if load_in_ram:
+            self.data_pc, self.label_pc = self.preprocess()
+
+    def __getitem__(self, index):
+        if self.load_in_ram:
+            return index, self.data_pc[index], self.label_pc[index]
+        else:
+            data, center, scale = self.load_data(index)
+            label = self.load_label(index, center, scale)
+            return index, data, label
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def preprocess(self):
+        data_samples = []
+        label_samples = []
+        for idx in range(len(self.data_list)):
+            data, center, scale = self.load_data(idx)
+            label = self.load_label(idx, center, scale)
+            data_samples.append(data)
+            label_samples.append(label)
+        return data_samples, label_samples
+
+    def load_data(self, index):
+        fpath = self.data_list[index]
+        surf_reader = vtk.vtkXMLPolyDataReader()
+        surf_reader.SetFileName(fpath)
+        surf_reader.Update()
+        surf = surf_reader.GetOutput()
+        surf_pts = numpy_support.vtk_to_numpy(surf.GetPoints().GetData())
+        pointSet = np.array(surf_pts)        
+        # Randomly sample desired number of points
+        if pointSet.shape[0] <= self.point_num:
+            raise AssertionError("Low number of surface points in org point cloud!")
+        idxs = np.random.randint(pointSet.shape[0], size=self.point_num)
+        pointSet = pointSet[idxs, :]
+        # Normalize the pointSet to be zero mean and inside unit sphere
+        center = np.mean(pointSet, axis=0)
+        pointSet -= center
+        max_dist = np.max(np.linalg.norm(pointSet, axis=1))
+        pointSet /= max_dist
+        return pointSet, center, max_dist
+    
+    def load_label(self, index, center, scale):
+        fpath = self.label_list[index]
+        srep_reader = vtk.vtkXMLPolyDataReader()
+        srep_reader.SetFileName(fpath)
+        srep_reader.Update()
+        srep = srep_reader.GetOutput()
+        srep_pts = numpy_support.vtk_to_numpy(srep.GetPoints().GetData())
+        pointSet = np.array(srep_pts) 
+        pointSet -= center
+        pointSet /= scale
+        return pointSet
