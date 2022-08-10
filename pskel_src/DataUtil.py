3888b30a9f77c6be7c26f8005e606f13a1ec9b55
Ninad Khargonkar <ninad.khargonkar@kitware.com>
2022-08-10 17:57:30 -0500

    unified srep class for metrics computation

--- pskel_src/DataUtil.py
+++ pskel_src/DataUtil.py
@@ -235,3 +235,113 @@ class LeafletData(Dataset):
         pointSet -= center
         pointSet /= scale
         return pointSet
+
+
+class Srep:
+
+    def __init__(self, from_json=True, json_f=None, vtp_dir=None):
+        if from_json:
+            if not json_f:
+                raise ValueError("No valid json file provided!")
+            skel_pts, up_spokes, dn_spokes, crest_pts, crest_spokes = self.extract_srep_json(json_f)
+        else:
+            if not json_f:
+                raise ValueError("No valid dir provided for vtp files!")
+            skel_pts, up_spokes, dn_spokes, crest_pts, crest_spokes = self.extract_srep_vtp(vtp_dir)
+        
+        self.all_points = np.concatenate([skel_pts, crest_pts], axis=0)
+        self.skel_pts = skel_pts
+        self.crest_pts = crest_pts
+        self.up_spokes = up_spokes
+        self.down_spokes = dn_spokes
+        self.crest_spokes = crest_spokes
+
+    def get_implied_boundary_pts(self):
+        N = self.skel_pts.shape[0]
+        M = self.crest_pts.shape[0]
+        boundary_pts = np.zeros((2*N+M, 3))
+        # from the skel points and up/down spokes
+        for i in range(N):
+            idx_up = i
+            idx_dn = i + N
+            boundary_pts[idx_up] = self.skel_pts[i] + self.up_spokes[i]
+            boundary_pts[idx_dn] = self.skel_pts[i] + self.down_spokes[i]
+        # from the crest points
+        for i in range(M):
+            idx_crest = 2*N + i
+            boundary_pts[idx_crest] = self.crest_pts[i] + self.crest_spokes[i]
+        return boundary_pts
+
+    # vtp loading
+    def _util_extract_indiv_srep_vtp(self, fpath):
+        reader = vtk.vtkXMLPolyDataReader()
+        reader.SetFileName(fpath)
+        reader.Update()
+        polydata = reader.GetOutput()
+        points = numpy_support.vtk_to_numpy(polydata.GetPoints().GetData())
+        pointdata = polydata.GetPointData()
+        directions = numpy_support.vtk_to_numpy(pointdata.GetArray("spokeDirection"))
+        radii = numpy_support.vtk_to_numpy(pointdata.GetArray("spokeLength"))
+        spokes = np.expand_dims(radii, axis=1) * directions
+        return np.array(points), np.array(spokes), np.array(radii), np.array(directions)
+
+    def extract_srep_vtp(self, data_dir):
+        up_f = os.path.join(data_dir, "up_proc.vtp")
+        down_f = os.path.join(data_dir, "down_proc.vtp")
+        crest_f = os.path.join(data_dir, "crest_proc.vtp")
+        p_up, up_spokes, r_up, d_up = self._util_extract_indiv_srep_vtp(up_f)
+        p_dn, dn_spokes, r_dn, d_dn = self._util_extract_indiv_srep_vtp(down_f)
+        p_cr, cr_spokes, r_cr, d_cr = self._util_extract_indiv_srep_vtp(crest_f)
+        return p_up, up_spokes, dn_spokes, p_cr, cr_spokes
+
+
+    # Json loading
+    def _util_get_data_from_inner_list(self, inner_list):
+        skel_points = []
+        up_spokes = []
+        dn_spokes = []
+        crest_points = []
+        crest_spokes = []
+        for item in inner_list:
+            spoke_pt = item['UpSpoke']['SkeletalPoint']['Value']
+            up_dir = item['UpSpoke']['Direction']['Value']
+            dn_dir = item['DownSpoke']['Direction']['Value']
+            skel_points.append(spoke_pt)
+            up_spokes.append(up_dir)
+            dn_spokes.append(dn_dir)
+            if 'CrestSpoke' in item:
+                crest_pt = item['CrestSpoke']['SkeletalPoint']['Value']
+                crest_dir = item['CrestSpoke']['Direction']['Value']
+                crest_points.append(crest_pt)
+                crest_spokes.append(crest_dir)
+        return skel_points, up_spokes, dn_spokes, crest_points, crest_spokes
+
+    def _util_get_data_from_skeleton(self, skeleton_list):
+        all_skel_points = []
+        all_up_spokes = []
+        all_dn_spokes = []
+        all_crest_points = []
+        all_crest_spokes = []
+        # skel_points, up_spokes, dn_spokes, crest_points, crest_spokes
+        all_data = [self._util_get_data_from_inner_list(inner_list) 
+                        for inner_list in skeleton_list]
+        for skel_points, up_spokes, dn_spokes, crest_points, crest_spokes in all_data:
+            all_skel_points += skel_points
+            all_up_spokes += up_spokes
+            all_dn_spokes += dn_spokes
+            all_crest_points += crest_points
+            all_crest_spokes += crest_spokes
+        return all_skel_points, all_up_spokes, all_dn_spokes, all_crest_points, all_crest_spokes
+
+    def extract_srep_json(self, fpath):
+        with open(fpath, "r") as f:
+            data = json.load(f)
+        srepdata = data['EllipticalSRep']
+        skeldata = srepdata['Skeleton']
+        srep_data = self._util_get_data_from_skeleton(skeldata)
+        skel_pts = np.array(srep_data[0])
+        up_spokes = np.array(srep_data[1])
+        dn_spokes = np.array(srep_data[2])
+        crest_pts = np.array(srep_data[3])
+        crest_spokes = np.array(srep_data[4])
+        return skel_pts, up_spokes, dn_spokes, crest_pts, crest_spokes
